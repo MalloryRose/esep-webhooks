b2d51904d9c6bfb9e784579b2fd5076ba736ca93
MalloryRose <116680613+MalloryRose@users.noreply.github.com>
2025-04-09 15:37:28 -0400

    Test

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -18,27 +18,16 @@ public class Function
     /// <returns></returns>
     public string FunctionHandler(object input, ILambdaContext context)
     {
-        context.Logger.LogInformation($"FunctionHandler received: {input}");
-
         dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
-
-        // Testing from postman, you can use this code to test the function
-        /*
-        context.Logger.LogInformation($"Body: {json.body}");
-        dynamic body = JsonConvert.DeserializeObject<dynamic>(json.body.ToString());
-        context.Logger.LogInformation($"Issue: {body.issue}");
-        context.Logger.LogInformation($"Html: {body.issue.html_url}");
-        string payload = $"{{'text':'Issue Created: {body.issue.html_url}'}}";
-        */
         
         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
-
+        
         var client = new HttpClient();
         var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
         {
             Content = new StringContent(payload, Encoding.UTF8, "application/json")
         };
-    
+
         var response = client.Send(webRequest);
         using var reader = new StreamReader(response.Content.ReadAsStream());
             
--- EsepWebhook/src/EsepWebhook/obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
+++ EsepWebhook/src/EsepWebhook/obj/Release/net8.0/linux-x64/EsepWebhook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6becaa35975cafc9c97ef53d4532b469b3e89369")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
